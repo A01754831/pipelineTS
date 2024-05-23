519babd9af03b338d6e253342e2cd6dccf4e0a69
Alonso Segura <alonsoseg26@outlook.com>
2024-05-23 15:54:53 -0600

    Esqueleto

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import Server from './provider/Server';
+import {PORT,NODE_ENV} from './config';
+import express from 'express';
+import cors from 'cors';
+
+
+const server = new Server({
+    port:PORT,
+    env:NODE_ENV,
+    middlewares:[
+        express.json(),
+        express.urlencoded({extended:true}),
+        cors()
+    ],
+    controllers:[
+        // AgenteController.instance,
+        // AuthenticationController.instance
+    ]
+});
+
+//Extendiendo la interfaz Request de Express para poder acceder a los datos del usuario
+declare global {
+    namespace Express {
+        interface Request {
+            user: string;
+            token: string;
+        }
+    }
+}
+
+server.init();
